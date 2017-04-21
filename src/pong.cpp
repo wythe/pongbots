7067093dbae626b5c560f73e32d1a1f7121a46e1
Mark Beckwith <mark@intrig.com>
2017-04-21 09:44:27 -0500

    adding ball_type

--- src/pong.cpp
+++ src/pong.cpp
@@ -11,9 +11,21 @@ const double pi = std::acos(-1);
 const int W = 800;
 const int H = 600;
 
+struct ball_type {
+    ball_type(sf::Vector2f size, sf::Vector2f angle) : b(size), d(angle) {}
+    ball_type(const ball_type &) = default;
+    ball_type & operator=(const ball_type &) = default;
+    sf::RectangleShape b;
+    sf::Vector2f d;
+};
+
 template <typename T>
 sf::IntRect to_rect(T const & shape) {
     return sf::IntRect(shape.getPosition().x, shape.getPosition().y, shape.getSize().x, shape.getSize().y);
+}
+
+sf::IntRect to_rect(const ball_type & b) {
+    return to_rect(b.b);
 }
 
 template <typename Shape>
@@ -21,9 +33,17 @@ int mid_y(const Shape & rect) {
     return rect.getPosition().y + rect.getSize().y / 2;
 }
 
+int mid_y(const ball_type & b) {
+    return mid_y(b.b);
+}
+
 template <typename Shape>
 int mid_x(const Shape & rect) {
     return rect.getPosition().x + rect.getSize().x / 2;
+}
+
+int mid_x(ball_type & b) {
+    return mid_x(b.b);
 }
 
 template <typename Shape>
@@ -31,13 +51,21 @@ int set_midpoint(Shape & rect, int x, int y) {
     rect.setPosition(x - rect.getSize().x / 2, y - rect.getSize().y / 2);
 }
 
+int set_midpoint(ball_type & rect, int x, int y) {
+    set_midpoint(rect, x, y);
+}
+
 template <typename Shape>
 int set_midpoint(Shape & rect, int y) {
     rect.setPosition(rect.getPosition().x, y - rect.getSize().y / 2);
 }
 
-template <typename Shape>
-int ai_update(const Shape & ball, const Shape & paddle) {
+int set_midpoint(ball_type & b, int y) {
+    set_midpoint(b.b, y);
+}
+
+template <typename Ball, typename Shape>
+int ai_update(const Ball & ball, const Shape & paddle) {
     if (mid_y(ball) > mid_y(paddle)) return 1;
     return -1;
 }
@@ -107,8 +135,8 @@ void bounce(const sf::IntRect & ball, const sf::IntRect & wall, sf::Vector2f & d
     // sound.play();
 }
 
-template <typename T>
-void bounce(const T & ball, const T & wall, sf::Vector2f & dir, sf::Sound & sound) {
+template <typename Ball, typename T>
+void bounce(const Ball & ball, const T & wall, sf::Vector2f & dir, sf::Sound & sound) {
     bounce(to_rect(ball), to_rect(wall), dir, sound);
 }
 
@@ -151,10 +179,17 @@ int main(int argc, char* argv[]) {
         // ball, direction, and sound
         sf::RectangleShape ball(sf::Vector2f(15, 15));
         sf::Vector2f dir(45 * 2 * pi / 360.f, 45 * 2 * pi / 360.f); // 45 degree angle
+
+        auto b2 = ball_type {
+            sf::Vector2f(15, 15), 
+            sf::Vector2f(45 * 2 * pi / 360.f, 45 * 2 * pi / 360.f) }; // 45 degree angle;
+    
+
         sf::SoundBuffer sound_buff;
         if (!sound_buff.loadFromFile("assets/ball.wav")) throw std::runtime_error("cannot open sound file");
         sf::Sound sound(sound_buff);
         set_midpoint(ball, 400, 20);
+
 
         // font 
         sf::Font font;
